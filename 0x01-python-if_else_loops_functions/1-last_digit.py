6c3ed78358ca44a5ca54d953ecde174605f42be7
= <soufian.kamma@gmail.com>
2023-11-28 06:56:39 -0800

    Task 1.The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
     last_digit = -last_digit
 if last_digit > 5:
     stat = "and is greater than 5"
-elif last_digit < 6 and not(last_digit == 0):
+elif not(last_digit == 0) and last_digit < 6:
     stat = "and is less than 6 and not 0"
 elif last_digit == 0:
     stat = "and is 0"
