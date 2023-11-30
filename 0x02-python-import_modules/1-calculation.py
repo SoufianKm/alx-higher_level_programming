d38fd042e01af3208d9c86e81a6996f13446222d
= <soufian.kamma@gmail.com>
2023-11-30 11:18:36 -0800

    Task 1.My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """does adding, substraction, multiplication
-    division Maths of 10 and 5, and prints the result"""
+    """does some  Maths of 10 and 5, and prints the result"""
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
