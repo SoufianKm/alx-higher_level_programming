f9b00e852a5ea5dd79ca8025ce5d16ec1d38829c
= <soufian.kamma@gmail.com>
2023-11-30 08:46:14 -0800

    Task 5.Everything can be imported

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ imports the variable a from the file
+    variable_load_5.py and prints its value."""
+    import variable_load_5 as var
+    print("{}".format(var.a))
