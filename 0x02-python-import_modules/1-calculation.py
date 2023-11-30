27933b38c0beff36af5c8c4a5828097e5bc55c77
= <soufian.kamma@gmail.com>
2023-11-30 04:08:17 -0800

    Task 1.My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """does + - * / Maths, and prints the result"""
+    """does add, sub, mul, div Maths, and prints the result"""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
