73c8270575b3bf27ca354184714bb99b8e1efba5
= <soufian.kamma@gmail.com>
2023-12-01 11:54:32 -0800

    Task 3.Print a list of integers... in reverse!

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """prints all integers of a list, in reverse order
+
+    Args:
+        my_list: list of integers
+
+    Return:
+        0 always.
+    """
+    my_list.reverse()
+    for nbr in my_list:
+        print("{}".format(nbr))
