70582ff8bcd823fd0e87717e58492c93180301cf
= <soufian.kamma@gmail.com>
2023-12-01 13:15:03 -0800

    Task 5.Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """removes all characters c and C from a string.
+
+    Args:
+        my_string: the string given
+
+    Return:
+        new string.
+    """
+    tmp_list = list(my_string)
+    for c in tmp_list:
+        if c in 'cC':
+            tmp_list.remove(c)
+
+    return "".join(tmp_list)
