02ca95f7addd85dad1522f3f5a2195e0f7fd073c
= <soufian.kamma@gmail.com>
2023-12-02 04:04:45 -0800

    Task 3.Print a list of integers... in reverse!

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,7 +9,7 @@ def print_reversed_list_integer(my_list=[]):
     Return:
         0 always.
     """
-    if len(my_list) > 0:
+    if my_list:
         my_list.reverse()
         for nbr in my_list:
             print("{:d}".format(nbr))
