c1f8694d0fc1692ef9b707301b2f5cd756e28dfa
= <soufian.kamma@gmail.com>
2023-12-06 06:03:10 -0800

    Task 5.Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """returns the number of keys in a dictionary
+
+    Args:
+        a_dictionary: a dictionary given
+
+    Return:
+        number of keys
+    """
+    return len(a_dictionary)
