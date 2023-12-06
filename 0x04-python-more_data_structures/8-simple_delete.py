aba0a3362e25f1e734af13331ab714a96392e16f
= <soufian.kamma@gmail.com>
2023-12-06 06:40:56 -0800

    Task 8.Simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhton3
+
+def simple_delete(a_dictionary, key=""):
+    """deletes a key in a dictionary.
+
+    Args:
+        a_dictionary: dictionary given
+        key: argument of dictionary
+
+    Return:
+        new dictionary
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+
+    return a_dictionary
