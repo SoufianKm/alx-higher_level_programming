57db99cc0e8363dd7d79f650ef71154c080b4dc1
= <soufian.kamma@gmail.com>
2023-12-06 15:39:55 -0800

    Task 11.Multiply by using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -11,4 +11,5 @@ def multiply_list_map(my_list=[], number=0):
     Return:
         new list
     """
-    return list(map(lambda nbr: nbr * number, my_list))
+    new_list = list(map(lambda nbr: nbr * number, my_list))
+    return new_list
