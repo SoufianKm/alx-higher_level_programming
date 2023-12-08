cebf1f79ae16dcfe04373755f6e896c57bf15d3b
= <soufian.kamma@gmail.com>
2023-12-08 06:41:58 -0800

    Task 101-Squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda element: list(map(lambda nbr: nbr * nbr, element)), matrix))
+    return list(map(lambda x: list(map(lambda n: n * n, x)), matrix))
