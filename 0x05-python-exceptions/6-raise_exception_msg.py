cd3ad63ba685ea3cdab64335c1f93de0b19e9f52
= <soufian.kamma@gmail.com>
2023-12-18 10:54:31 -0800

    Task 6.Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """raises a name exception with a message.
+
+    Args:
+        message: a message to be shown
+    """
+    print(message)
