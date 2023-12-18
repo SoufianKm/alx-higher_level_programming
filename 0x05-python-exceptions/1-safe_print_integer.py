d0f41133d4d308b71dc1f180f47abaee80ff769e
= <soufian.kamma@gmail.com>
2023-12-18 06:25:38 -0800

    Task 1.Safe printing of an integers list

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """prints an integer with "{:d}".format().
+
+    Args:
+        value: can contain any type (integer, string, etc.)
+
+    Return:
+        True if value has been correctly printed, Otherwise False.
+    """
+    try:
+        print("{:d}".format(int(value)), end="\n")
+        return True
+    except ValueError:
+        return False
