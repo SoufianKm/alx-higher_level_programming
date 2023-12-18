cf04f239f13fbeabdd80844f387351673ae3b0c8
= <soufian.kamma@gmail.com>
2023-12-18 14:36:04 -0800

    Task 8. Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    """executes a function safely.
+
+    Args:
+        args: list of argument
+
+    Return:
+        the result of the function. Otherwise, returns None
+    """
+    try:
+        res = fct(*args)
+    except BaseException as e:
+        res = None
+        print("Exception: {}".format(e), file=sys.stderr)
+    finally:
+        return res
