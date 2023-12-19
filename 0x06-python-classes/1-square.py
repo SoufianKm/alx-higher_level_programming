c5be4002b1dc2d713f18e3091372ca3b32e2810c
= <soufian.kamma@gmail.com>
2023-12-19 13:39:58 -0800

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module defines a empty class square """
+
+
+class Square:
+    """ class defines a square """
+    def __init__(self, size):
+        """ Initialize class """
+        self.__size = size
