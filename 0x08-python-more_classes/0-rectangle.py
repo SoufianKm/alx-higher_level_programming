98d3a8b719647ffaedfe332b6cef537f49835c60
= <soufian.kamma@gmail.com>
2024-01-04 12:02:24 -0800

    Task 0.Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,9 +3,10 @@
 This is a module for a class Rectangle
 """
 
-class Rectangle:
-   """class of rectangle"""
 
-   def __init__(self):
+class Rectangle:
+    """class of rectangle"""
+
+    def __init__(self):
         """initialise class"""
         pass
