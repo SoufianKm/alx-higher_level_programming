7e9444b7f340a04283288a35bab22de1eb067f1d
= <soufian.kamma@gmail.com>
2024-01-09 14:02:59 -0800

    Task 1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''
-Module to append text to file
+Module to write to file
 '''
 
 
-def append_write(filename="", text=""):
-    ''' Appends text to file '''
-    with open(filename, 'a') as open_file:
-        count = open_file.write(text)
+def write_file(filename="", text=""):
+    ''' Writes text to file '''
+    with open(filename, 'w') as open_file:
+        open_file.write(text)
+        count = open_file.tell()
     return count
