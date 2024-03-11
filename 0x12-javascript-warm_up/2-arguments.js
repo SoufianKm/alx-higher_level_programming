708ff657c5dd5f1203c74acb771f7489864102f9
SoufianKm <soufian.kamma@gmail.com>
2024-03-11 05:42:00 -0700

    Task 2 -  Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 /*
  * a script that prints a message depending of the number
  * of arguments passed:
- * 	If no arguments are passed to the script, print “No argument”
- * 	If only one argument is passed to the script, print “Argument found”
- * 	Otherwise, print “Arguments found”
+ *    If no arguments are passed to the script, print “No argument”
+ *    If only one argument is passed to the script, print “Argument found”
+ *    Otherwise, print “Arguments found”
  */
 
 const count = process.argv.length;
