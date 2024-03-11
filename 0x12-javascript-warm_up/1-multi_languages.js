bca829e5e99b33308b4988d9a9a41e93472bf8cd
SoufianKm <soufian.kamma@gmail.com>
2024-03-11 05:23:55 -0700

    Task 1 - 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ * a script that prints 3 lines
+ */
+let text = 'C is fun';
+console.log(text);
+text = 'Python is cool';
+console.log(text);
+text = 'JavaScript is amazing';
+console.log(text);
