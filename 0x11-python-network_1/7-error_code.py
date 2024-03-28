7b3b3f4e69650ebad95d48e9d65be00162f5abcc
SoufianKm <soufian.kamma@gmail.com>
2024-03-28 10:34:47 -0700

    Task 7 -  Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to
+the URL and displays the body of the response.
+"""
+
+
+if __name__ == "__main__":
+
+    try:
+        import requests
+        from sys import argv
+
+        response = requests.get(argv[1])
+        if response.status_code >= 400:
+            print("Error code: {}".format(response.status_code))
+        else:
+            print(response.text)
+    except Exception as err:
+        pass
