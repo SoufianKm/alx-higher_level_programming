3093b04ba92acbecda3a30bfe018412324d0c3ff
SoufianKm <soufian.kamma@gmail.com>
2024-04-23 08:44:23 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script that display the status code of a GET request.
+//
+//   The first argument is the URL to request (GET)
+//   The status code must be printed like this: code: <status code>
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error)
+    console.log(error);
+  console.log('code:' + ' ' + response.statusCode);
+});
